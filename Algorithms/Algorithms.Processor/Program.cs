7f71509a8992576099befa8ac6c82f210519c7f8
aodegov <a.odegov@svitla.com>
2020-07-06 00:27:53 +0300

    Solution initialization

---
+++ Algorithms/Algorithms.Processor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
